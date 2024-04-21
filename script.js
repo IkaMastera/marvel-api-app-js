e5607383b0990ef5cef2b15c88b7a75caf97b127
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-21 13:39:17 +0400

    add: character image styles

--- script.js
+++ script.js
@@ -4,7 +4,36 @@ let showContainer = document.getElementById("show-container");
 let listContainer = document.querySelector(".list");
 
 let date = new Date();
+
 const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
+
+function removeElements() {
+  listContainer.innerHTML = "";
+}
+
+input.addEventListener("keyup", async () => {
+  removeElements();
+  if (input.value.length < 4) {
+    return false;
+  }
+
+  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${input.value}`;
+
+  const response = await fetch(url);
+  const jsonData = await response.json();
+
+  jsonData.data["results"].forEach((result) => {
+    let name = result.name;
+    let div = document.createElement("div");
+    div.style.cursor = "pointer";
+    div.classList.add("autocomplete-items");
+    div.setAttribute("onclick", "displayWords('" + "')");
+    let word = "<b>" + name.substr(0, input.value.length) + "</b>";
+    word += name.substr(input.value.length);
+    div.innerHTML = `<p class="item">${word}</p>`;
+    listContainer.appendChild(div);
+  });
+});
 
 button.addEventListener(
   "click",
